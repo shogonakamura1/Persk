97cbfbf736d0be0b4c44f0ac1c7d7de1aae5ab67
Nakamura Shogo <nakamurashogo@NakamuranoMacBook-Air.local>
2025-09-19 17:35:58 +0900

    ローカル開発用設定を追加
    - ALLOWED_HOSTSにlocalhostと127.0.0.1を追加
    - STATIC_ROOTを設定してcollectstaticコマンドを有効化
    - ローカルサーバーでの動作を改善

--- persk/settings.py
+++ persk/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-m%9)m&6m39nob2!785$9*&w^-^db6s$ez0d=hc63=wh9sbtdo+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
